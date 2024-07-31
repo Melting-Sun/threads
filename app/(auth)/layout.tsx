b7b2a83a8d6489cf762c779514483c254523eb45
erfan <98186931+Melting-Sun@users.noreply.github.com>
2024-07-31 13:04:53 +0330

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,27 @@
+import { ClerkProvider } from '@clerk/nextjs'
+import { Inter } from 'next/font/google'
+import React, { ReactNode } from 'react'
+import '../../app/globals.css'
+
+interface Props {
+    children: ReactNode
+}
+
+const inter = Inter({subsets: ['latin']})
+
+export const metadata = {
+    title: 'Threds',
+    description: 'Threads with NextJs'
+}
+
+export default function layout({children}: Props) {
+  return (
+    <ClerkProvider>
+        <html lang='en'>
+            <body className={`${inter.className} bg-dark-1`}>
+                {children}
+            </body>
+        </html>
+    </ClerkProvider>
+  )
+}
