544eea9abbe14034ded52618149eda824f9c6cfa
erfan <98186931+Melting-Sun@users.noreply.github.com>
2024-08-06 11:55:13 +0330

    more commits

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -2,12 +2,13 @@ import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import {dark} from '@clerk/themes'
 
 export default function Topbar() {
   return (
     <nav className="topbar">
       <Link href={"/"} className="flex items-center gap-4">
-        <Image src="/logo.svg" alt="logo" width={28} height={28} />
+        <Image src="/assets/threads-white-icon.svg" alt="logo" width={28} height={28} />
         <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
       </Link>
 
@@ -28,6 +29,7 @@ export default function Topbar() {
         </div>
         <OrganizationSwitcher
           appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: "py-2 px-4",
             },
