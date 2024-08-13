f355c5ece97392bd6cc8a85843a3efda0e8c80f5
erfan <98186931+Melting-Sun@users.noreply.github.com>
2024-08-13 18:33:55 +0330

    center login-register form

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,27 +1,29 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import { Inter } from 'next/font/google'
-import React, { ReactNode } from 'react'
-import '../../app/globals.css'
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import React, { ReactNode } from "react";
+import "../../app/globals.css";
 
 interface Props {
-    children: ReactNode
+  children: ReactNode;
 }
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: 'Threds',
-    description: 'Threads with NextJs'
-}
+  title: "Threds",
+  description: "Threads with NextJs",
+};
 
-export default function layout({children}: Props) {
+export default function layout({ children }: Props) {
   return (
     <ClerkProvider>
-        <html lang='en'>
-            <body className={`${inter.className} bg-dark-1`}>
-                {children}
-            </body>
-        </html>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>
+          <div className="w-full flex justify-center items-center min-h-screen">
+            {children}
+          </div>
+        </body>
+      </html>
     </ClerkProvider>
-  )
+  );
 }
