2b6d377649314bdc6c33a30d7def025af9e94cad
erfan <98186931+Melting-Sun@users.noreply.github.com>
2024-08-14 17:49:34 +0330

    ignoreBuildErrors

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-      eslint: {
-        ignoreDuringBuilds: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
       },
-    images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "img.clerk.com",
-          },
-          {
-            protocol: "https",
-            hostname: "images.clerk.dev",
-          },
-          {
-            protocol: "https",
-            hostname: "uploadthing.com",
-          },
-          {
-            protocol: "https",
-            hostname: "placehold.co",
-          },
-        ],
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
       },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
